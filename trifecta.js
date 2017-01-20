83e4890ef75c146bea29a33a0d6142fc20446337
Ash <ash@ashryanbeats.com>
2017-01-20 18:50:19 +0900

    Add comments.

--- trifecta.js
+++ trifecta.js
@@ -1,17 +1,22 @@
+// Node modules
 const Promise = require("bluebird");
 const fs = require("fs");
 const Git = require("nodegit");
 const ncp = Promise.promisify(require('ncp').ncp);
 const path = require("path");
 
+// Paths
 const destination = process.cwd();
 const source = path.join(__dirname, "./generated");
 
 
+// Set concurrency limit
 ncp.limit = 16;
 
+// Copy from source to destination
 ncp(source, destination)
 	.then(function() {
+		// Git init
 		return Git.Repository.init(destination, 0);
 	})
 	.then(function(repo) {
